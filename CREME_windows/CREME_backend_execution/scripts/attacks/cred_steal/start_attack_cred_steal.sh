84cd6dafa2b7cb51dd19b26b03c8388ca8e20e64
jackson910210 <32348727+jackson910210@users.noreply.github.com>
2021-10-01 22:26:30 +0800

    script to start cred_steal

---
+++ CREME_windows/CREME_backend_execution/scripts/attacks/cred_steal/start_attack_cred_steal.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/expect -f
+
+set delKnownHosts [lindex $argv 0]
+set kali_ip [lindex $argv 1]
+set username [lindex $argv 2]
+set password [lindex $argv 3]
+set path "/root/times"
+set target_ip [lindex $argv 4]
+
+
+set timeout 15
+
+spawn /bin/bash $delKnownHosts
+send "exit\r"
+
+spawn ssh $username@$kali_ip
+expect "*continue connecting (yes/no*)? "
+send "yes\r"
+expect " password: "
+send "$password\r"
+
+
+expect "#"
+#send "python3 /root/Desktop/cred_steal/cred_steal.py\r"
+send "python3 /root/Desktop/cred_steal/cred_steal_FirstStage.py $path $kali_ip $target_ip\r"
+
+sleep 120
+
+expect "#"
+send "cd Desktop\r"
+
+expect "#"
+send "exit\r"
+
+
