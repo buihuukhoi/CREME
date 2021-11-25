eafc7769ad0f697704cbe82fceb5e37e74456c99
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2021-11-25 17:02:24 +0800

    小修

--- CREME_backend_execution/classes/CREME.py
+++ CREME_backend_execution/classes/CREME.py
@@ -1213,7 +1213,7 @@ class Creme:
                                                                 final_name_atop, time_window_traffic)
         # balance data and filter features
         ProcessDataHelper.balance_data(folder_atop, final_name_atop)
-        ProcessDataHelper.balance_data(folder_traffic, final_name_traffic)
+        ProcessDataHelper.balance_data(folder_traffic, final_name_traffic, balanced_label_zero = False)
         ProcessDataHelper.filter_features(folder_atop, final_name_atop, 0.1)
         ProcessDataHelper.filter_features(folder_traffic, final_name_traffic, 0.04)
         ProgressHelper.update_stage(stage, f"Finished processing the accounting and network packet data sources", 5,
